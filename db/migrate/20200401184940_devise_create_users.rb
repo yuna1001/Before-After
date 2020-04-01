7f21a12b32c9bfc594b6298153bff42341aa564d
yuna1001 <hegapoo1001@gmail.com>
2020-04-01 20:13:58 +0000

    [Add] マイグレーションファイルにUser関連のカラムを追加後DBresetを実行

--- db/migrate/20200401184940_devise_create_users.rb
+++ db/migrate/20200401184940_devise_create_users.rb
@@ -34,6 +34,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
 
       t.string :name
       t.timestamps null: false
+      t.text :body
+      t.string :image_id
+      t.boolean :admin
     end
 
     add_index :users, :email,                unique: true
