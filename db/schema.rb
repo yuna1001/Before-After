c7baf7ea9515c20afa442169a7f323abe1fa5322
yuna1001 <hegapoo1001@gmail.com>
2020-04-03 14:05:56 +0000

    [Add] Followモデルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_03_125123) do
+ActiveRecord::Schema.define(version: 2020_04_03_135842) do
 
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -26,6 +26,16 @@ ActiveRecord::Schema.define(version: 2020_04_03_125123) do
     t.string "after_image_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id", null: false
+    t.integer "followed_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["followed_id"], name: "index_relationships_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
   end
 
   create_table "users", force: :cascade do |t|
