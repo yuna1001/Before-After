f6001b37a66da2624435565d07b8674adcc8b8ff
yuna1001 <hegapoo1001@gmail.com>
2020-04-03 09:35:30 +0000

    [Add] 投稿一覧ページを作成

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'c60373dd392675301704b6001e3981a2f2f36c8edb7a27e71bb3b703f0a86115e686d67ac0d8c76bbdcc84febe75c3858a831ae0de6a536ba840e67b11c5bfc8'
