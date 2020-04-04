d9c53255b1ca8980690ae57e716db937c90fa1a7
yuna1001 <hegapoo1001@gmail.com>
2020-04-04 23:36:15 +0000

    [Add] 投稿一覧ページのいいね機能にAjax処理を導入

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,18 +1,14 @@
 class FavoritesController < ApplicationController
   def create
-    post = Post.find(params[:post_id])
-    favorite = current_user.favorites.new(post_id: post.id)
+    @post = Post.find(params[:post_id])
+    favorite = current_user.favorites.new(post_id: @post.id)
     favorite.save
-    @url = request.referer
-    redirect_to @url
   end
 
   def destroy
-    post = Post.find(params[:post_id])
-    favorite = current_user.favorites.find_by(post_id: post.id)
+    @post = Post.find(params[:post_id])
+    favorite = current_user.favorites.find_by(post_id: @post.id)
     favorite.destroy
-    @url = request.referer
-    redirect_to @url    
   end
 
 end
