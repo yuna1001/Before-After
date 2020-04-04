486d258a4661ced18c6a0d27555cd7118f2acd89
yuna1001 <hegapoo1001@gmail.com>
2020-04-04 18:21:27 +0000

    [Add] Topページを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+
+  root 'home#top'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
   resources :users, only: [:show,  :new, :edit, :update] do
     member do
       get :following, :followers
