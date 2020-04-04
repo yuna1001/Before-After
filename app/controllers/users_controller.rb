eb7ba45e8d1191aabe0aab624feb011c8467b371
yuna1001 <hegapoo1001@gmail.com>
2020-04-04 17:36:10 +0000

    [Add] フォロー中ユーザの一覧ページを作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,16 @@ class UsersController < ApplicationController
     end
   end
 
+  def following
+    @user = User.find(params[:id])
+    @users = @user.following
+  end
+
+  def followers
+    @user = User.find(params[:id])
+    @users = @user.followers
+  end
+
   private
   def user_params
     params.require(:user).permit(:name, :body, :email)
