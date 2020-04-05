9becee21017a3bba0132b55ea4234f2688f7b30b
yuna1001 <hegapoo1001@gmail.com>
2020-04-05 07:14:05 +0000

    [Fix] 未ログインユーザが新規投稿できないように修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user! , only:[:create, :edit, :update]
+  before_action :authenticate_user! , only:[:new, :create, :edit, :update]
   
   def index
     if params[:ranking]
