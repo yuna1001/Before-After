69869c006a58a7a48006b8a6869730dbb2a48edb
yuna1001 <hegapoo1001@gmail.com>
2020-04-28 14:02:20 +0900

    [Fix] ユーザ詳細ページの投稿一覧が新着順で表示されない箇所を修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,6 +34,9 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @posts = 
+      Post.where(user_id: @user.id)
+      .order("created_at DESC")
   end
 
   def edit
